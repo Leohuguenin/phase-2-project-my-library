77bd5a55a980eb5c99a58910cbeb5b4d8b165081
Leohuguenin <leonardohuguenin@live.com>
2025-07-17 11:06:14 +1000

    Add: App component with routing setup

---
+++ src/Components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Sort from "./Sort";
+import ReadingList from "./ReadingList";
+import Search from "./Search";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/sort" component={Sort} />
+        <Route path="/reading-list" component={ReadingList} />
+        <Route path="/search" component={Search} />
+        <Route path="*">
+          <h2>Page not found</h2>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
