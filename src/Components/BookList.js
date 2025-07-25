9ebdb3bdb3b5b1e09785101ae21336dd6876c720
Leohuguenin <leonardohuguenin@live.com>
2025-07-25 12:13:22 +1000

    Add pagination to BookList component
    Introduced pagination with 'Back' and 'Next' buttons to display books in sets of four. The BookList now uses local state to manage the visible range of books.

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import BookCard from "./BookCard";
 
 
 function BookList({ books }) {
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(4);
+
+    function handleNextButtonClick() {
+        setStartIndex(startIndex + 4);
+        setEndIndex(endIndex + 4);
+    }
+    
+    function handleBackButtonClick() {
+        if (startIndex !== 0) {
+            setStartIndex(startIndex - 4);
+            setEndIndex(endIndex - 4);
+        }
+    }
+
+
     return (
         <>
             <h2>Featured Books</h2>
             <div className="book-list">
-                {books.map(book => <BookCard key={book.id} book={book}/>
+                <button onClick={handleBackButtonClick} >Back button</button>
+                {books.slice(startIndex, endIndex).map(book => <BookCard key={book.id} book={book}/>
                 )}
+                <button onClick={handleNextButtonClick} >Next button</button>
             </div>
         </>
     )
